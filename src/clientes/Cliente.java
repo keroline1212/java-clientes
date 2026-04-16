f22bd49a9cf29a67cf30c00225f0d0743487a838
keroline1212 <kerolineluthke@gmail.com>
2026-04-16 15:28:36 -0300

    organizando em pacotes

--- src/clientes/Cliente.java
+++ src/clientes/Cliente.java
@@ -1,4 +1,4 @@
-package empresa;
+package clientes;
 
 public class Cliente {
     String nome;
--- src/clientes/Main.java
+++ src/clientes/Main.java
@@ -1,4 +1,4 @@
-package empresa;
+package clientes;
 
 import java.util.ArrayList;
 import java.util.Scanner;
